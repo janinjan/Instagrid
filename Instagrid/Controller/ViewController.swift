53311d092bfc74e6d137b6700d3d1622a777ce94
janinjan <janin.culhaoglu@gmail.com>
2019-03-25 00:54:10 +0100

    Added gestures recognizers, method to share photo grid, extensions to pick an image.
    Gestures :
    - tap to update pictures
    - swipe up of left to share photo grid

--- Instagrid/Controller/ViewController.swift
+++ Instagrid/Controller/ViewController.swift
@@ -10,10 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // MARK: - Outlets
     @IBOutlet var mainView: LayoutView!
-    
+    @IBOutlet weak var swipeLayout: UIStackView!
     @IBOutlet var layoutsButton: [UIButton]!
     
+    // MARK: - Properties
+    private var swipeGesture = UISwipeGestureRecognizer()
+    var tag: Int?
+    
+    // MARK: - Actions
     @IBAction func layoutButtonsTapped(_ sender: UIButton) {
         layoutsButton.forEach({ $0.isSelected = false })
         sender.isSelected = true
@@ -29,8 +35,91 @@ class ViewController: UIViewController {
         }
     }
     
+    @IBAction func picturesButtonsTapped(_ sender: UIButton) {
+        tag = sender.tag
+        pickAnImage()
+    }
+    
+    // MARK: - Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         mainView.layoutStyle = .layout2
+        tapGesturesRecognizer()
+        swipeGestureDirectionRecognizer()
+    }
+    
+    private func pickAnImage() {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.allowsEditing = false
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    private func swipeLayoutSetToIdentity() {
+        UIView.animate(withDuration: 0.3, animations: {
+            self.mainView.transform = .identity
+        })
+    }
+    
+    func handleShare() {
+        UIGraphicsBeginImageContext(mainView.frame.size)
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+            mainView.layer.render(in: context)
+        guard let imageToShare = UIGraphicsGetImageFromCurrentImageContext() else { return }
+        
+        let activityVC = UIActivityViewController(activityItems: [imageToShare], applicationActivities: nil)
+        present(activityVC, animated: true, completion: nil)
+        activityVC.completionWithItemsHandler = {(activityType: UIActivity.ActivityType?, success: Bool, returnedItems: [Any]?, error: Error?) in
+            if success || !success {
+                self.swipeLayoutSetToIdentity()
+            }
+        }
+    }
+    
+    // MARK: - Gestures
+    func tapGesturesRecognizer() {
+        mainView.picturesCollectionImageView.forEach( { $0.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(picturesCollectionImageViewTapped(gesture:)))) })
+    }
+    
+    @objc func picturesCollectionImageViewTapped(gesture: UIGestureRecognizer) {
+        tag = gesture.view?.tag
+        pickAnImage()
+    }
+    
+    func swipeGestureDirectionRecognizer() {
+        let directions: [UISwipeGestureRecognizer.Direction] = [.up, .left]
+        for direction in directions {
+            swipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(mainViewSwiped(gesture:)))
+            mainView.addGestureRecognizer(swipeGesture)
+            swipeGesture.direction = direction
+            mainView.isUserInteractionEnabled = true
+        }
+    }
+    
+    @objc func mainViewSwiped(gesture: UISwipeGestureRecognizer) {
+        UIView.animate(withDuration: 0.3) {
+            if gesture.direction == .up && UIDevice.current.orientation.isPortrait {
+                self.mainView.transform = CGAffineTransform(translationX: 0, y: -self.view.frame.height)
+                print("swipe up")
+                self.handleShare()
+            } else if gesture.direction == .left && UIDevice.current.orientation.isLandscape {
+                self.mainView.transform = CGAffineTransform(translationX: -self.view.frame.width, y: 0)
+                print("swipe left")
+                self.handleShare()
+            }
+        }
+    }
+}
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            guard let tag = tag else { return }
+            mainView.picturesCollectionImageView[tag].image = image
+            mainView.picturesButton[tag].isHidden = true
+        }
+        dismiss(animated: true, completion: nil)
     }
 }
